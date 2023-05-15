8610cf7acc956399098a0cdeb40720a211d8c240
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-05-14 23:06:30 -0400

    edited server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express'); // Import ApolloServer
+const { typeDefs, resolvers } = require('./schemas'); // Import your typeDefs and resolvers
 const db = require('./config/connection');
-const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth'); // Import your auth middleware
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware, // Add your authentication middleware to the context
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +25,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
